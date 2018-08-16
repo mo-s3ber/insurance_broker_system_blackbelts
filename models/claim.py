72e57c4f533a2d6b619cf270db01d45e8da33e25
Mohamed Saber <mohamedabosaber94@gmail.com>
2018-08-16 17:08:11 +0200

    claim risk up

--- models/claim.py
+++ models/claim.py
@@ -5,11 +5,20 @@ class claimPolicy(models.Model):
     _name ="insurance.claim"
 
     policy_number = fields.Many2one('policy.broker',string='Policy Number',required=True)
-    renewal = fields.Char(string='Renewal', required=True)
-    endorsement= fields.Char(string='Endorsement', required=True)
+    risk_object=fields.Char(string='Risk Object')
+    renewal = fields.Many2one('renewal.again',string='Renewal', required=True,domain="[('old_number','=',policy_number)]")
+    endorsement= fields.Many2one('endorsement.edit',string='Endorsement', required=True,domain="[('number_policy','=',policy_number)]")
     risk_person = fields.Many2one('person.object',string='Person Risk')
     risk_vehicle=fields.Many2one('vehicle.object',string='Vehicle Risk')
     risk_cargo = fields.Many2one('cargo.object', string='Cargo Risk')
     coverage = fields.Many2one('insurance.product.coverage',string='Coverage')
     details= fields.Char(string='Details')
     amount = fields.Float(string='Amount')
+
+    @api.onchange('policy_number')
+    def _onchange_policy_number(self):
+        if self:
+            self.risk_object = self.policy_number.line_of_bussines.object
+        else:
+            self.risk_object = 0
+
